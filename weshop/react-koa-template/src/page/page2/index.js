538be11fde88b8cd2c488919891b2216ba4214f2
zhanzhan <zhanzhan@MacBook-Pro.local>
2020-01-17 19:49:28 +0800

    0.0.2

--- weshop/react-koa-template/src/page/page2/index.js
+++
@@ -1,6 +0,0 @@
-import React, { Component } from 'react'
-
-export default function Index() {
-    return <div>page2~</div>
-}
-ReactDOM.render( <Index/>, document.getElementById('app') );
--- weshop/react-koa-template/src/promise3.js
+++
@@ -1,5 +0,0 @@
-new Promise((resolve, reject) => {
-    resolve(1)
-}).then((value) => {
-    console.log(value)
-}) 
